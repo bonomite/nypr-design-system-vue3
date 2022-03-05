f9abb501d74d513a7f109774e03970926bad911d
Thomas Bono <thomasbono.com@gmail.com>
2022-03-05 03:04:52 -0500

    started typography css var replace for darkmode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,23 @@ import { app, addParameters } from '@storybook/vue3'
 // import './storybook-styles.scss'
 import { action } from '@storybook/addon-actions'
 
+import addons from '@storybook/addons'
+
+// get an instance to the communication channel for the manager and preview
+const channel = addons.getChannel()
+
+// switch body class for story along with interface theme
+channel.on('DARK_MODE', isDark => {
+  if (isDark) {
+    document.body.classList.add('dark')
+    document.body.setAttribute('data-theme', 'dark')
+  } else {
+    document.body.classList.remove('dark')
+    document.body.setAttribute('data-theme', 'default')
+  }
+})
+
+
 addParameters({
   docs: {
     inlineStories: true,
